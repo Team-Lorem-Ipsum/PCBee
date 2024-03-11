d0b9b8f8a3b7f9b3f0554c49d04a692c58921ebf
0Thomas1 <thomas2001114@gmail.com>
2024-03-11 13:26:41 -0700

    updated Oauth to server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,52 @@
         const data = await response.json()
         return data
     } 
+    //ebay OAuth
+    const scopes = ['https://api.ebay.com/oauth/api_scope',
+    'https://api.ebay.com/oauth/api_scope/sell.marketing.readonly',
+    'https://api.ebay.com/oauth/api_scope/sell.marketing',
+    'https://api.ebay.com/oauth/api_scope/sell.inventory.readonly',
+    'https://api.ebay.com/oauth/api_scope/sell.inventory',
+    'https://api.ebay.com/oauth/api_scope/sell.account.readonly',
+    'https://api.ebay.com/oauth/api_scope/sell.account',
+    'https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly',
+    'https://api.ebay.com/oauth/api_scope/sell.fulfillment'
+];
+        // pass the credentials through constructor
+    ebayAuthToken = new EbayAuthToken({
+      clientId: 'MatthewW-PCBee-PRD-8d10abed2-1575fa5e',
+      clientSecret: 'PRD-d10abed232bd-dd2d-46cd-9cd7-52fb',
+      redirectUri: 'Matthew_Widjaja-MatthewW-PCBee--ozkizh'
+    });
 
+    const clientScope = 'https://api.ebay.com/oauth/api_scope';
+    // // Client Crendential Auth Flow
+    ebayAuthToken.getApplicationToken('PRODUCTION', clientScope).then((data) => {
+      console.log(data);
+    }).catch((error) => {
+      console.log(`Error to get Access token :${JSON.stringify(error)}`);
+    });
+
+    // // Authorization Code Auth Flow
+    let code = ebayAuthToken.generateUserAuthorizationUrl('PRODUCTION', scopes); // get user consent url.
+    // Using user consent url, you will be able to generate the code which you can use it for exchangeCodeForAccessToken.
+    
+    // // Exchange Code for Authorization token
+    ebayAuthToken.exchangeCodeForAccessToken('PRODUCTION', code).then((data) => { // eslint-disable-line no-undef
+      console.log(data);
+    }).catch((error) => {
+      console.log(error);
+      console.log(`Error to get Access token :${JSON.stringify(error)}`);
+    });
+
+    // // Getting access token from refresh token obtained from Authorization Code flow
+    const refreshToken = 'v^1.1#i^1#r^1#f^0#I^3#p^3#t^Ul4xMF8yOjNDMjU1MUI0OTJBMDg5NUZGMUY4RkEwNjk1MDRBQjQ2XzNfMSNFXjI2MA==';
+    ebayAuthToken.getAccessToken('PRODUCTION', refreshToken, scopes).then((data) => {
+      console.log(data);
+    }).catch((error) => {
+      console.log(`Error to get Access token from refresh token:${JSON.stringify(error)}`);
+    });
+    //ebay OAuth END
 
     /**
      * Middleware declarations
@@ -30,11 +75,7 @@
         response.send(`${config.ROOT}/index.html`)
     })
     
-    app.get('/countries', async(request, response) => {
-        if (!countries)
-            countries = await Utils.readJson(`${__dirname}/../data/countries.json`)
-        response.status(200).json(countries)
-    })
+    
    
 
    
