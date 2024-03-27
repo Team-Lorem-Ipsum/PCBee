5ce7f67adc6b0a4b21c5fc2dd251c79431637186
0Thomas1 <thomas2001114@gmail.com>
2024-03-26 22:42:41 -0700

    Add category ids to item-desci.js

--- client/js/item-desci.js
+++ client/js/item-desci.js
@@ -7,12 +7,22 @@
     let data = await response.json();
     return data;
   }
+  //category ids
+  const category_ids={
+    "gpu": 27386,
+    "cpu": 164,
+    "memory": 170083,
+    "motherboard": 1244,
+    "caseFan": 131486,
+    "case": 42014,
+    "monitor": 80053
+  };
 
   //get the category and item id from the url
   let url = new URLSearchParams(window.location.search);
   let catId = url.get("category");
   let itemId = url.get("itemId");
   console.log(catId, "itemid: ",itemId);
-  let data = getJSONData(`/popular/${catId}`);
+  let data = getJSONData(`/popular/${category_ids[catId]}`);
   console.log(data);
 })();
