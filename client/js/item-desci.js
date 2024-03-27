2422423ea21b3b6dae3d5af795fc7f4f70d26701
0Thomas1 <thomas2001114@gmail.com>
2024-03-26 22:15:16 -0700

    Add category and item id retrieval from URL parameters

--- client/js/item-desci.js
+++ client/js/item-desci.js
@@ -1,11 +1,18 @@
 
 (() =>{
+  //url = item-desc.html?category=:category&itemId=:itemId
   //get the data from the server
   const getJSONData = async (url) => {
     let response = await fetch(url);
     let data = await response.json();
     return data;
   }
+
+  //get the category and item id from the url
+  let url = new URLSearchParams(window.location.search);
+  let catId = url.get("category");
+  let itemId = url.get("itemId");
+  console.log(catId, "itemid: ",itemId);
   let data = getJSONData(`/popular/${catId}`);
   console.log(data);
 })();
