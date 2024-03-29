0fb2dc5c40273735b1b3bc3a52bac9a2c543f3d5
0Thomas1 <99365833+0Thomas1@users.noreply.github.com>
2024-03-28 17:28:18 -0700

    Fix authorization header in server.js

--- server/server.js
+++ server/server.js
@@ -83,7 +83,7 @@
 
       let response = await axios.get(`${url}?${q}&${limit}`, {
         headers: {
-          Authorization: `Bearer ${access_token}`, // TODO
+          Authorization: `Bearer ${access_token}`,
           "Content-Type": "application/json",
         },
       });
