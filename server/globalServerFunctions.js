93d8e85295824daedfc6bf34c820531d451adf5b
Kevin Nguyen <kevin.nguyen7100@gmail.com>
2024-03-29 21:48:57 -0700

    Updated server.test.js, created two unit tests for the chat gpt api

--- server/globalServerFunctions.js
+++ server/globalServerFunctions.js
@@ -5,19 +5,29 @@
 const axios = require("axios");
 
 const globalServerFunctions = {
+    
     GPT_CHAT_HISTORY: [{
         "role": "system",
         "content": `You are an assistant that helps explain PC parts, give PC builds with specific features (budget, range, fidelity), and guide users how to build PCs. You do not answer anything that isn't related to PC or PC parts.`
     }],
 
-    GPT_API_CALL: async function (message) {
+    GPT_API_CALL: async function (testMessage) {
         const apiUrl = "https://api.openai.com/v1/chat/completions";
         const apiKey = "";
 
         try {
             const response = await axios.post(apiUrl, {
                 model: "gpt-3.5-turbo-0125",
-                messages: globalServerFunctions.GPT_CHAT_HISTORY,
+                messages: testMessage != null ? [
+                    {
+                        "role": globalServerFunctions.GPT_CHAT_HISTORY[0].role,
+                        "content": globalServerFunctions.GPT_CHAT_HISTORY[0].content
+                    },
+                    {
+                        "role": "user",
+                        "content": testMessage,
+                    }
+                ] : globalServerFunctions.GPT_CHAT_HISTORY,
                 temperature: 0.7,
                 n: 1
             }, {
@@ -29,7 +39,6 @@ const globalServerFunctions = {
 
             return response
         } catch (Error) {
-
             console.error(Error.message);
             return null
         }
--- server/server.test.js
+++ server/server.test.js
@@ -11,12 +11,17 @@ test('Check if chatbot responds to user', async () => {
     }
 });
 
-// test('Check to see if the chatbot ', async () => {    
-//     try {
-//         const response = await serverFunctions.GPT_API_CALL()   ;
-//         expect(response).toBeDefined();
-//     } catch (error) {
-//         console.error('Error calling GPT API:', error);
-//         expect(error).toBeNull();
-//     }
-// });
+test('Check to see if the chatbot responds to irrelevant questions', async () => {    
+    try {
+        const response = await serverFunctions.GPT_API_CALL("What's 9 + 10? If this is an irrelevant question, please respond with False");
+        const completion = response.data.choices[0].message.content;
+        const Test = completion.match(/[false|FALSE]+/g)
+
+        expect(Test[0]).toBe("False");
+    } catch (error) {
+        // if (error)
+            // console.error('Error calling GPT API:', error);
+    
+        expect(error).toBeNull();
+    }
+});
