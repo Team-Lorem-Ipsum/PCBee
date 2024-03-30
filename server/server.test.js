702bf118bce77c8f0243168ee9416185567a7712
Kevin Nguyen <kevin.nguyen7100@gmail.com>
2024-03-30 15:38:57 -0700

    Updated server.test.js, added more unit tests for chatgpt api

--- server/server.test.js
+++ server/server.test.js
@@ -1,6 +1,10 @@
 const axios = require('axios');
 const serverFunctions = require('./globalServerFunctions');
 
+test('CHeck if the api call function exists', () => {
+    expect(serverFunctions.GPT_API_CALL).toBeDefined();
+})
+
 test('Check if chatbot responds to user', async () => {    
     try {
         const response = await serverFunctions.GPT_API_CALL();
@@ -11,13 +15,30 @@ test('Check if chatbot responds to user', async () => {
     }
 });
 
-test('Check to see if the chatbot responds to irrelevant questions', async () => {    
+test('Check to see if the chatbot responds properly to irrelevant questions', async () => {    
     try {
         const response = await serverFunctions.GPT_API_CALL("What's 9 + 10? If this is an irrelevant question, please respond with False");
         const completion = response.data.choices[0].message.content;
         const Test = completion.match(/[false|FALSE]+/g)
 
         expect(Test[0]).toBe("False");
+    } catch (error) {
+        // if (error)
+            // console.error('Error calling GPT API:', error);
+    
+        expect(error).toBeNull();
+    }
+});
+
+test('Check to see if the chatbot responds properly to relevant questions', async () => {    
+    try {
+        const response = await serverFunctions.GPT_API_CALL("Can you help me build a PC? If this is an relevant question, please respond with True");
+        const completion = response.data.choices[0].message.content;
+        const Test = completion.match(/[True|TRUE]+/g)
+        
+        console.log(Test[0])
+
+        expect(Test[0]).toBe("True");
     } catch (error) {
         // if (error)
             // console.error('Error calling GPT API:', error);
